b5bef9a6d6038a41643acb6549d1d28f84a640c7
Beynona <mr.optimuc1997@gmail.com>
2020-02-04 12:56:53 +0300

    Проект обучения - 1

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jenua";
+            int age = 22;
+            bool isEmployed = false;
+            double weight = 52.3;
+
+            Console.WriteLine();
+        }
+    }
+}
